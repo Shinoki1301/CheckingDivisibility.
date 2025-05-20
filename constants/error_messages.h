adcaf0229a9867970f7a61ee2b560c711b4fd147
Shinoki <shinoki1301@gmail.com>
2025-05-20 18:06:05 +0300

    Добавлен файл error_messages.h

---
+++ constants/error_messages.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_MESSAGES_H
+#define ERROR_MESSAGES_H
+
+// Сообщения об ошибках
+#define ERROR_NOT_FOUND          "Not Found"
+#define ERROR_METHOD_NOT_ALLOWED "Method Not Allowed"
+#define ERROR_INTERNAL_SERVER    "Internal Server Error"
+#define ERROR_DIVISION_BY_ZERO   "Ошибка: деление на ноль"
+
+#endif
