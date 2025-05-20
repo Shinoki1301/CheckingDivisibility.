5c653117ba29541a46f9d98db136a32e0417fccf
Shinoki <shinoki1301@gmail.com>
2025-05-20 19:46:25 +0300

    Обновлено форматирование файла router.c

--- router/router.c
+++ router/router.c
@@ -1,36 +1,38 @@
 #include "router.h"
+
 #include <string.h>
 
-// Структура, описывающая один маршрут: путь и связанный с ним идентификатор маршрута
+// Структура, описывающая один маршрут: путь и связанный с ним идентификатор
+// маршрута
 typedef struct {
-    const char* path;  // Путь (URI)
-    Route       route; // Какой маршрут ему соответствует
+    const char* path;   // Путь (URI)
+    Route       route;  // Какой маршрут ему соответствует
 } RouteEntry;
 
 // Таблица маршрутов
 static const RouteEntry route_table[] = {
-    { "/style.css", ROUTE_STYLES },
-    { "/logout",     ROUTE_LOGOUT },
-    { "/login",      ROUTE_LOGIN  },
-    { "/div",        ROUTE_DIV_FORM },
-    { "/div-form",   ROUTE_DIV_FORM },
-    { "/div-submit", ROUTE_DIV_SUBMIT },
-    { "/",           ROUTE_ROOT   },
+    {"/style.css",  ROUTE_STYLES },
+    {"/logout",     ROUTE_LOGOUT },
+    {"/login",      ROUTE_LOGIN  },
+    {"/div",        ROUTE_DIV_FORM },
+    {"/div-form",   ROUTE_DIV_FORM },
+    {"/div-submit", ROUTE_DIV_SUBMIT },
+    {"/",           ROUTE_ROOT },
 };
 
 // Определяет маршрут по URI HTTP-запроса
 Route resolve_route(struct mg_http_message* hm) {
     Route resolved = ROUTE_UNKNOWN;
 
-    size_t count = sizeof(route_table) / sizeof(route_table[0]); 
-    for (size_t i = 0; i < count; ++i) {
+    size_t count = sizeof (route_table) /
+	           sizeof (route_table[0]);
+    for (size_t iter = 0; iter < count; ++iter) {
         // Сравнение URI запроса с каждым путем в таблице
-        if (mg_strcmp(hm->uri, mg_str(route_table[i].path)) == 0) {
-            resolved = route_table[i].route;
-	    i = count;
+        if (mg_strcmp(hm->uri, mg_str (route_table[iter].path)) == 0) {
+            resolved = route_table[iter].route;
+            iter     = count;
         }
     }
     // Если путь не найден — возвращаем ROUTE_UNKNOWN
     return resolved;
 }
-
