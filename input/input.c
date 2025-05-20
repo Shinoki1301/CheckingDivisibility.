48268276efd55e17ff9e1a1d061903b87bc290d3
Shinoki <shinoki1301@gmail.com>
2025-05-20 19:44:34 +0300

    Обновлено форматирование файлов input/

--- input/input.c
+++ input/input.c
@@ -3,19 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* read_file (const char* filename) {
-    FILE* f   = fopen (filename, "rb");
+char* read_file(const char* filename) {
+    FILE* f   = fopen(filename, "rb");
     char* res = NULL;
     if (f) {
-        fseek (f, 0, SEEK_END);
-        size_t size = ftell (f);
-        rewind (f);
+        fseek(f, 0, SEEK_END);
+        size_t size = ftell(f);
+        rewind(f);
         res = (char*) malloc (size + 1);
         if (res) {
-            fread (res, 1, size, f);
+            fread(res, 1, size, f);
             res[size] = '\0';
         }
-        fclose (f);
+        fclose(f);
     }
     return res;
 }
