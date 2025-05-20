2760855f1d2494ca65592532f0545b8ee039e510
Shinoki <shinoki1301@gmail.com>
2025-05-20 17:42:55 +0300

    Добавлен файл http_status.h

---
+++ constants/http_status.h
@@ -0,0 +1,11 @@
+#ifndef HTTPS_STATUS_H
+#define HTTPS_STATUS_H
+
+#define HTTP_OK                 200
+#define HTTP_FOUND              302
+#define HTTP_BAD_REQUEST        400
+#define HTTP_METHOD_NOT_ALLOWED 405
+#define HTTP_NOT_FOUND          404
+#define HTTP_INTERNAL_ERROR     500
+
+#endif
